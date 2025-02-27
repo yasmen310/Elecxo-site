958cb4c01496211c782ea6568081389412d49a11
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2025-02-27 09:38:20 -0800

    Update HeroBanner.jsx

--- src/components/banner/HeroBanner.jsx
+++ src/components/banner/HeroBanner.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./HeroBanner.css";
+import "./herobanner.css";
 import { Link } from "react-router-dom";
 
 const HeroBanner = () => {
